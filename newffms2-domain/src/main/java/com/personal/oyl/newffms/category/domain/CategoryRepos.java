03ca92b3cb88104006f913c74f6a50d371a0211f
OuYang Liang <ouyanggod@gmail.com>
2017-11-27 23:06:10 +0800

    增加查询所有类别的方法

--- newffms2-domain/src/main/java/com/personal/oyl/newffms/category/domain/CategoryRepos.java
+++ newffms2-domain/src/main/java/com/personal/oyl/newffms/category/domain/CategoryRepos.java
@@ -14,20 +14,20 @@ import com.personal.oyl.newffms.common.NewffmsDomainException.NewffmsSystemExcep
 import com.personal.oyl.newffms.common.NewffmsDomainException.NoOperatorException;
 
 public interface CategoryRepos {
-	/**
-	 * 根据类别标识查询类别
-	 * 
-	 * @param key 类别标识
-	 * @return 类别实体
-	 * @throws CategoryKeyEmptyException
-	 */
-	Category categoryOfId(CategoryKey key) throws CategoryKeyEmptyException;
-	
-	/**
-	 * 创建新的类别（仅限根类别）
-	 * 
-	 * @param bean 待创建类别实体
-	 * @param operator 操作人
+    /**
+     * 根据类别标识查询类别
+     * 
+     * @param key 类别标识
+     * @return 类别实体
+     * @throws CategoryKeyEmptyException
+     */
+    Category categoryOfId(CategoryKey key) throws CategoryKeyEmptyException;
+
+    /**
+     * 创建新的类别（仅限根类别）
+     * 
+     * @param bean 待创建类别实体
+     * @param operator 操作人
      * @throws CategoryDescEmptyException
      * @throws CategoryDescTooLongException
      * @throws NoOperatorException
@@ -40,11 +40,11 @@ public interface CategoryRepos {
             NoOperatorException, CategoryNotRootException, CategoryBudgetEmptyException, CategoryBudgetInvalidException,
             NewffmsSystemException;
 
-	/**
-	 * 删除类别（必须为叶子类别）
-	 * 
-	 * @param key 待删除类别标识
-	 * @param operator 操作人
+    /**
+     * 删除类别（必须为叶子类别）
+     * 
+     * @param key 待删除类别标识
+     * @param operator 操作人
      * @throws CategoryKeyEmptyException
      * @throws NoOperatorException
      * @throws CategoryNotLeafException
@@ -53,20 +53,27 @@ public interface CategoryRepos {
      */
     void remove(CategoryKey key, String operator) throws CategoryKeyEmptyException, NoOperatorException,
             CategoryNotLeafException, CategoryNotExistException, NewffmsSystemException;
-	
-	/**
-	 * 根据父类别标识查询子类别
-	 * 
-	 * @param parentKey 父类别标识
-	 * @return 子类别集合（无排序）
-	 * @throws CategoryKeyEmptyException
-	 */
-	List<Category> categoriesOfParent(CategoryKey parentKey) throws CategoryKeyEmptyException;
-	
-	/**
-	 * 查询所有根类别
-	 * 
-	 * @return 根类别集合（无排序）
-	 */
-	List<Category> rootCategories();
+
+    /**
+     * 根据父类别标识查询子类别
+     * 
+     * @param parentKey 父类别标识
+     * @return 子类别集合（无排序）
+     * @throws CategoryKeyEmptyException
+     */
+    List<Category> categoriesOfParent(CategoryKey parentKey) throws CategoryKeyEmptyException;
+
+    /**
+     * 查询所有根类别
+     * 
+     * @return 根类别集合（无排序）
+     */
+    List<Category> rootCategories();
+    
+    /**
+     * 查询所有类别
+     * 
+     * @return 全量类别集合（无排序）
+     */
+    List<Category> allCategories();
 }
--- newffms2-domain/src/main/java/com/personal/oyl/newffms/category/domain/CategoryReposImpl.java
+++ newffms2-domain/src/main/java/com/personal/oyl/newffms/category/domain/CategoryReposImpl.java
@@ -20,157 +20,167 @@ import com.personal.oyl.newffms.common.NewffmsDomainException.NoOperatorExceptio
 
 public class CategoryReposImpl implements CategoryRepos {
 	
-	@Autowired
-	private CategoryMapper mapper;
+    @Autowired
+    private CategoryMapper mapper;
 
-	@Override
-	public Category categoryOfId(CategoryKey key) throws CategoryKeyEmptyException {
-		if (null == key || null == key.getCategoryOid()) {
-			throw new CategoryKeyEmptyException();
-		}
-		
-		Category param = new Category();
-		param.setKey(key);
-		
-		List<Category> list = mapper.select(param);
-		
-		if (null == list || list.isEmpty()) {
-			return null;
-		}
-		
-		return list.get(0);
-	}
+    @Override
+    public Category categoryOfId(CategoryKey key) throws CategoryKeyEmptyException {
+        if (null == key || null == key.getCategoryOid()) {
+            throw new CategoryKeyEmptyException();
+        }
+
+        Category param = new Category();
+        param.setKey(key);
+
+        List<Category> list = mapper.select(param);
+
+        if (null == list || list.isEmpty()) {
+            return null;
+        }
+
+        return list.get(0);
+    }
 
     @Override
     public void add(Category bean, String operator) throws CategoryDescEmptyException, CategoryDescTooLongException,
             NoOperatorException, CategoryNotRootException, CategoryBudgetEmptyException, CategoryBudgetInvalidException,
             NewffmsSystemException {
- 
-	    if (null == bean || null == bean.getCategoryDesc() || bean.getCategoryDesc().trim().isEmpty()) {
+
+        if (null == bean || null == bean.getCategoryDesc() || bean.getCategoryDesc().trim().isEmpty()) {
             throw new CategoryDescEmptyException();
         }
 
         if (bean.getCategoryDesc().trim().length() > 10) {
             throw new CategoryDescTooLongException();
         }
-        
+
         if (null == bean.getLeaf()) {
             bean.setLeaf(true);
         }
-        
+
         if (null == bean.getCategoryLevel()) {
             bean.setCategoryLevel(Integer.valueOf(1));
         }
-        
-        if (!bean.getLeaf() || bean.getCategoryLevel() != 1 ) {
+
+        if (!bean.getLeaf() || bean.getCategoryLevel() != 1) {
             throw new CategoryNotRootException();
         }
-        
+
         if (null == bean.getMonthlyBudget()) {
             throw new CategoryBudgetEmptyException();
         }
-        
+
         if (bean.getMonthlyBudget().compareTo(BigDecimal.ZERO) <= 0) {
             throw new CategoryBudgetInvalidException();
         }
-	    
-		if (null == operator || operator.trim().isEmpty()) {
-			throw new NoOperatorException();
-		}
-		
-		bean.setCreateBy(operator);
-		bean.setCreateTime(new Date());
-		
-		int n = mapper.insert(bean);
-		
-		if (1 != n) {
+
+        if (null == operator || operator.trim().isEmpty()) {
+            throw new NoOperatorException();
+        }
+
+        bean.setCreateBy(operator);
+        bean.setCreateTime(new Date());
+
+        int n = mapper.insert(bean);
+
+        if (1 != n) {
             throw new NewffmsSystemException();
         }
-		
-		bean.setSeqNo(1);
-	}
+
+        bean.setSeqNo(1);
+    }
 
     @Override
     public void remove(CategoryKey key, String operator) throws CategoryKeyEmptyException, NoOperatorException,
             CategoryNotLeafException, CategoryNotExistException, NewffmsSystemException {
         if (null == key || null == key.getCategoryOid()) {
-			throw new CategoryKeyEmptyException();
-		}
-		
-		if (null == operator || operator.trim().isEmpty()) {
-			throw new NoOperatorException();
-		}
-		
-		Category obj = this.categoryOfId(key);
-		
-		if (null == obj) {
-			throw new CategoryNotExistException();
-		}
-		
-		if (!obj.getLeaf()) {
-			throw new CategoryNotLeafException();
-		}
-		
-		//TODO check if current category has been used already.
-		
-		int n = mapper.delete(key);
-		
-		if (1 != n) {
+            throw new CategoryKeyEmptyException();
+        }
+
+        if (null == operator || operator.trim().isEmpty()) {
+            throw new NoOperatorException();
+        }
+
+        Category obj = this.categoryOfId(key);
+
+        if (null == obj) {
+            throw new CategoryNotExistException();
+        }
+
+        if (!obj.getLeaf()) {
+            throw new CategoryNotLeafException();
+        }
+
+        // TODO check if current category has been used already.
+
+        int n = mapper.delete(key);
+
+        if (1 != n) {
             throw new NewffmsSystemException();
         }
-		
-		CategoryKey parentKey = obj.getParentKey();
-		
-		while (null != parentKey) {
-			Category parent = this.categoryOfId(parentKey);
-			
-			if (null == parent) {
-				throw new IllegalStateException();
-			}
-			
-			List<Category> children = this.categoriesOfParent(parent.getKey());
-			
-			if (null == children || children.isEmpty()) {
-				parent.changeBudget(parent.getMonthlyBudget().subtract(obj.getMonthlyBudget()), true, operator);
-			} else {
-				parent.changeBudget(parent.getMonthlyBudget().subtract(obj.getMonthlyBudget()), null, operator);
-			}
-			
-			parentKey = parent.getParentKey();
-		}
-		
-	}
 
-	@Override
-	public List<Category> categoriesOfParent(CategoryKey parentKey) throws CategoryKeyEmptyException {
-		if (null == parentKey || null == parentKey.getCategoryOid()) {
-			throw new CategoryKeyEmptyException();
-		}
-		
-		Category param = new Category();
-		param.setParentKey(parentKey);
-		
-		List<Category> list = mapper.select(param);
-		
-		if (null == list || list.isEmpty()) {
-			return null;
-		}
-		
-		return list;
-	}
+        CategoryKey parentKey = obj.getParentKey();
 
-	@Override
-	public List<Category> rootCategories() {
-		Category param = new Category();
-		param.setCategoryLevel(Integer.valueOf(1));
-		
-		List<Category> list = mapper.select(param);
-		
-		if (null == list || list.isEmpty()) {
-			return null;
-		}
-		
-		return list;
-	}
+        while (null != parentKey) {
+            Category parent = this.categoryOfId(parentKey);
+
+            if (null == parent) {
+                throw new IllegalStateException();
+            }
+
+            List<Category> children = this.categoriesOfParent(parent.getKey());
+
+            if (null == children || children.isEmpty()) {
+                parent.changeBudget(parent.getMonthlyBudget().subtract(obj.getMonthlyBudget()), true, operator);
+            } else {
+                parent.changeBudget(parent.getMonthlyBudget().subtract(obj.getMonthlyBudget()), null, operator);
+            }
+
+            parentKey = parent.getParentKey();
+        }
+
+    }
+
+    @Override
+    public List<Category> categoriesOfParent(CategoryKey parentKey) throws CategoryKeyEmptyException {
+        if (null == parentKey || null == parentKey.getCategoryOid()) {
+            throw new CategoryKeyEmptyException();
+        }
+
+        Category param = new Category();
+        param.setParentKey(parentKey);
+
+        List<Category> list = mapper.select(param);
+
+        if (null == list || list.isEmpty()) {
+            return null;
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<Category> rootCategories() {
+        Category param = new Category();
+        param.setCategoryLevel(Integer.valueOf(1));
+
+        List<Category> list = mapper.select(param);
+
+        if (null == list || list.isEmpty()) {
+            return null;
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<Category> allCategories() {
+        List<Category> list = mapper.select(null);
+        if (null == list || list.isEmpty()) {
+            return null;
+        }
+
+        return list;
+    }
 
 }

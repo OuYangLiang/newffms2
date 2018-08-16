0ee168f1d5cd01b1aee7d44a4467c2f7c567e1c2
OuYang Liang <ouyanggod@gmail.com>
2018-08-16 13:20:02 +0800

    Add a parameter for the bash script to specify the container ip.

--- make-image-and-start-container.sh
+++ make-image-and-start-container.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ];then
+    echo "Usage: make-image-and-start-container.sh ip-address"
+    exit 1
+fi
+
 mvn -Dmaven.test.skip=true clean package
 
-docker image build -t newffms:1.0 .
+docker image build -t newffms:1.0 --build-arg IP_ADDRS=$1 .
 
 docker container run -d -p 8080:8080 -p 9000:9000 newffms:1.0
